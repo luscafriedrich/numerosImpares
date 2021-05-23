835dd4c3476ff3bfbc060d79e427b312262e5d23
Lucas Friedrich <ntc.lucassilva@gmail.com>
2021-05-23 10:48:57 -0300

    Projeto criado dia 23/05/21 - Exercicio lógica, subindo para Github...

---
+++ src/numeroImpares/numerosImpares.java
@@ -0,0 +1,18 @@
+package numeroImpares;
+
+/**
+ *
+ * @author Lucas
+ */
+public class numerosImpares {
+    public static void main(String[] args) {
+    int cont;
+
+    cont = 101;
+    
+    while (cont<=200){
+        System.out.println("Numeros impares: " +cont);
+        cont = cont + 2;
+    }
+    } 
+}
